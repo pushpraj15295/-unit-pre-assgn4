99c090751519638ec2956f95fa60b922c8f38b7c
Pushpraj Patel <101568029+pushpraj15295@users.noreply.github.com>
2022-10-20 10:28:06 +0530

    Add files via upload

---
+++ Deccan-Herald-clone-project/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes,Route } from 'react-router-dom';
+
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import  Navbar  from './Components/Navbar';
+
+import Entertentment from './Pages/Entertentment';
+import Business from './pages1/Business';
+import National from './pages1/National';
+import Science from './pages1/Science';
+import Technology from './pages1/Technology';
+import Us from './pages1/Us';
+import Sport from './pages1/Sport';
+
+
+
+function App() {
+  return (
+    <> 
+    <div className='App'>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path='/entertentment' element={<Entertentment />} />
+      <Route path='/business' element={<Business />} />
+      <Route path='/national' element={<National />} />
+      <Route path='/science' element={<Science />} />
+      <Route path='/technology' element={<Technology />} />
+      <Route path='/us' element={<Us />} />
+      <Route path='/sport' element={<Sport />} />
+    </Routes>
+        
+        <Footer/>
+        </div>
+    </>
+  );
+}
+
+export default App;
